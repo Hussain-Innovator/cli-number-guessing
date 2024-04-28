47375db7c69a9280d9cae6868a4b9c941a189dd5
hussain1177 <hussainsamdani5686@gmail.com>
2024-04-29 02:18:30 +0500

    created Guessing Game

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/bash/env node   
+
+import inquirer from "inquirer";
+
+// 1) Computer will generate a random number
+
+// 2) User input for guessing number
+
+// 3) Compare User input with computer generated number and show result
+
+const randomNumber = Math.floor(Math.random() * 6+1);
+
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number between 1-6",
+    },
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congratulations! you guessed right number.")
+}else{
+    console.log("Sorry! You guessed wrong number.")
+}
